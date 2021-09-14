7ec8fade588504ffab3627e825ad68d5d2291e09
MatejSmith <mtjkovac@gmail.com>
2021-09-14 09:24:23 +0200

    initial

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,18 @@
 package com.appslab;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        multiplyByLength(new int[]{1,2,5});
+    }
+
+    static int[] multiplyByLength(int[] array) {
+        int x = 0;
+        for (int i : array) {
+            x = i;
+        }
+
     }
 }
