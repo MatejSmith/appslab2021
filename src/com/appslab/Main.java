6808ac3ff71675b547b3333190bb93bc43d012c2
MatejSmith <mtjkovac@gmail.com>
2021-09-14 09:56:56 +0200

    to be continued

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,18 +1,25 @@
 package com.appslab;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        multiplyByLength(new int[]{1,2,5});
+        System.out.println(multiplyByLength(new int[]{1, 5, 2, 4, 7, 1}));
     }
 
-    static int[] multiplyByLength(int[] array) {
+    static int multiplyByLength(int[] array) {
         int x = 0;
+        int z = 0;
         for (int i : array) {
             x = i;
         }
+        /*for (int i = 0; i < array.length; i++) {
+            array[i] = i * x;
+        }*/
 
+        for (int a = 0; a < array.length; a++) {
+            z = a;
+        }
+
+        return z;
     }
 }
