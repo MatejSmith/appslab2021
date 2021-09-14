39c93fee3d64c4ab8150b64159f6159dd0b1f705
Matej <mtjkovac@gmail.com>
2021-09-14 15:42:02 +0200

    Finish Multiply_by_Length exercise

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -5,14 +5,17 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        multiplyByLength(new int[]{1,2,5});
+        System.out.println(Arrays.toString(multiplyByLength(new int[]{1, 4, 6, 3, 7, 5})));
     }
 
     static int[] multiplyByLength(int[] array) {
         int x = 0;
-        for (int i : array) {
-            x = i;
+        for (int a = 0; a < array.length; a++) {
+            x = a + 1;
         }
-
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] * x;
+        }
+        return array;
     }
 }
