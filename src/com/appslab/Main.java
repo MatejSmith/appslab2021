2dc5dad40277cea4d07c6fa8a5216e646b707c35
MatejSmith <mtjkovac@gmail.com>
2021-09-14 08:36:57 +0200

    finish Triangle exercise

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,15 @@
 package com.appslab;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        if (!Triangle(4, 5, 9)) {
+            System.out.println("False");
+        } else {
+            System.out.println("True");
+        }
+    }
+
+    public static boolean Triangle(int a, int b, int c) {
+        return a + b > c && a + c > b && b + c > a;
     }
 }
