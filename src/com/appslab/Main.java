ac466ed7b93d7c2ba7fb9cab054f65a932c120ee
MatejSmith <mtjkovac@gmail.com>
2021-09-14 08:34:20 +0200

    finish Equality exercise

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,18 @@
 package com.appslab;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(Equal(2,3,4));
+    }
+
+    public static int Equal(int a, int b, int c)
+    {
+        int numberOfEquals = 0;
+        if (a == b && b ==c ) {
+            numberOfEquals = 3;
+        } else if (a == b || a == c || b == c) {
+            numberOfEquals = 2;
+        }
+        return numberOfEquals;
     }
 }
