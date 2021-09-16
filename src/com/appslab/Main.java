454cda6385346439cab9ba1cd87df2c650fb52ca
MatejSmith <mtjkovac@gmail.com>
2021-09-16 08:56:43 +0200

    finish War_of_Numbers exercise

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,20 +3,26 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println(Function((new int[]{44, 32, 86, 19})));
+    System.out.println(War(new int[]{12, 90, 75}));
     }
-
-    static int Function(int[] array) {
-        int min = array[0];
-        int max = array[0];
-        for (int j : array) {
-            if (j < min)
-                min = j;
+    static int War(int[] array) {
+        int odd = 0;
+        int even = 0;
+        for (int i : array) {
+            if (i % 2 == 0) {
+                odd = odd + i;
+            }
         }
         for (int i : array) {
-            if (i > max)
-                max = i;
+            if (i % 2 != 0) {
+                even = even + i;
+            }
         }
-            return (max) - (min);
+        if (even > odd) {
+            return (even) - (odd);
+        }
+        else {
+            return (odd) - (even);
+        }
     }
 }
