93cc3e7309db9b25c9ef4158721000a95f277f7b
MatejSmith <mtjkovac@gmail.com>
2021-09-18 09:01:30 +0200

    Finish Capture_the_rook exercise

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,10 +3,23 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Ending("appslab", "lab"));
-        System.out.println(Ending("appslab", "Lab"));
+        System.out.println((canCapture(new String[]{"A8", "F8"})));
     }
-    static boolean Ending(String full, String ending){
-        return full.endsWith(ending);
+
+    static boolean canCapture(String[] array) {
+        String First = "", Second = "", Third = "", Fourth = "";
+        for (int i = 1; i < array[0].length(); i++) {
+            First = First + array[0].charAt(i);
+        }
+        for (int i = 1; i < array[1].length(); i++) {
+            Second = Second + array[1].charAt(i);
+        }
+        for (int i = 0; i < array[0].length() - 1; i++) {
+            Third = Third + array[0].charAt(i);
+        }
+        for (int i = 0; i < array[1].length() - 1; i++) {
+            Fourth = Fourth + array[1].charAt(i);
+        }
+        return First.equals(Second) || Third.equals(Fourth);
     }
 }
